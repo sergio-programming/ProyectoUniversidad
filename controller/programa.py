f7e72fc6a8833ac6095dea63064b4ec2fb510eb0
sergio-programming <sergio0310@live.com>
2024-12-09 00:06:11 -0500

    Se implementa el componente de los Programas y se realizan mejoras de las otras abstracciones

---
+++ controller/programa.py
@@ -0,0 +1,23 @@
+from model.programa import Programa, ProgramaDao
+
+class ProgramaController:
+    
+    def registerPrograma(nombre, facultad_id):
+        programa = Programa(nombre, facultad_id)
+        ProgramaDao.create(programa)
+        
+    def getAllProgramas():
+        programas = []
+        programas = ProgramaDao.readAll()
+        return programas
+    
+    def getProgramaById(id):
+        programa = ProgramaDao.read(id)
+        return programa
+    
+    def updatePrograma(id, nombre, facultad_id):
+        programa = Programa(id, nombre, facultad_id)
+        ProgramaDao.update(programa)
+        
+    def deletePrograma(id):
+        ProgramaDao.delete(id)
